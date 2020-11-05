f2570301d687b19688e47e52aa2d31efdd4721f3
mezzzi <36529256+mezzzi@users.noreply.github.com>
2020-11-05 20:41:49 +0300

    Revert "feat: hvm translator stage one"
    This reverts commit 522d25c2080083bcd2fa1c8ece79108501654132.

--- src/abstractions/software/vm-translator/command/exception.js
+++
@@ -1,9 +0,0 @@
-/**
- * Error encountered while processing a given line/command
- */
-class CommandException extends Error {
-  constructor (message, lineNumber) {
-    super(`${lineNumber ? `In line ${lineNumber}: ` : ''}${message}`)
-  }
-}
-export default CommandException
